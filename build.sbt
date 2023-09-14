ea2b2191ced38e7b8fd8a6c619902c399d48eac8
Alexander Litvinenko <alekslitvinenk@gmail.com>
2023-09-14 17:08:38 +0300

    Use http4s client

--- build.sbt
+++ build.sbt
@@ -2,12 +2,22 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.12"
 
+val http4sVersion = "0.23.23"
+
 lazy val root = (project in file("."))
   .settings(
     name := "github-stars-counter",
+    scalacOptions ++= Seq(
+      "-feature",
+      "-deprecation",
+      "-unchecked",
+      "-language:postfixOps"
+    ),
     libraryDependencies ++= Seq(
       "eu.timepit" %% "fs2-cron-cron4s" % "0.8.3" withSources() withJavadoc(),
       "org.typelevel" %% "cats-effect" % "3.5.1" withSources() withJavadoc(),
-      "co.fs2" %% "fs2-core" % "3.9.1" withSources() withJavadoc()
+      "co.fs2" %% "fs2-core" % "3.9.1" withSources() withJavadoc(),
+      "org.http4s" %% "http4s-ember-client" % http4sVersion,
+      "org.http4s" %% "http4s-dsl"          % http4sVersion,
     )
   )
--- src/main/scala/io/dockovpn/githubstars/Main.scala
+++ src/main/scala/io/dockovpn/githubstars/Main.scala
@@ -3,20 +3,24 @@ package io.dockovpn.githubstars
 import cats.effect.{IO, IOApp, Temporal}
 import cron4s.Cron
 import eu.timepit.fs2cron.cron4s.Cron4sScheduler
-import fs2.concurrent.SignallingRef
 import fs2.Stream
+import fs2.concurrent.SignallingRef
+import org.http4s.client.JavaNetClientBuilder
 
-import java.time.LocalTime
 import scala.concurrent.duration.DurationInt
 
 object Main extends IOApp.Simple {
   
-  private val printTime = Stream.eval(IO(println(LocalTime.now)))
+  private val httpClient = JavaNetClientBuilder[IO].create
+  private val getStars = Stream.eval(
+    httpClient.expect[String]("https://ip.dockovpn.io")
+      .flatMap(IO.println)
+  )
   
   override def run: IO[Unit] = {
     val cronScheduler = Cron4sScheduler.systemDefault[IO]
     val evenSeconds = Cron.unsafeParse("*/2 * * ? * *")
-    val scheduled = cronScheduler.awakeEvery(evenSeconds) >> printTime
+    val scheduled = cronScheduler.awakeEvery(evenSeconds) >> getStars
     val cancel = SignallingRef[IO, Boolean](false)
     
     for {
